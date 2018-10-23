adff8d2f0040cfe8693db41979f72487c5b3363f
lyq998 <lyq998@users.noreply.github.com>
2018-10-23 20:00:30 +0800

    emmm
    i did few days ago

--- src/avefil.py
+++ src/avefil.py
@@ -5,26 +5,21 @@ if __name__ == '__main__':
     path = 'D:/aviris_hyperspectral_data/19920612_AVIRIS_IndianPine_EW-line_R_Gaussnoise.tif'
     imgarr = np.zeros((1848, 614, 220), dtype=np.uint16)
     imgarr = np.transpose(TIFF.open(path, mode="r").read_image())
-    newimgarr = np.zeros((200, 200, 220), dtype=np.uint16)
+    newimgarr = np.zeros((300, 300, 220), dtype=np.uint16)
 
     for i in range(220):
         print('success! %d' % i)
-        for j in range(200):
-            for k in range(200):
+        for j in range(300):
+            for k in range(300):
                 newimgarr[k, j, i] = (imgarr[k][j][i] / 9 + imgarr[k + 1][j][i] / 9 + imgarr[k + 2][j][i] / 9
                                       + imgarr[k][j + 1][i] / 9 + imgarr[k +
                                                                          1][j + 1][i] / 9 + imgarr[k + 2][j + 1][i] / 9
                                       + imgarr[k][j + 2][i] / 9 + imgarr[k + 1][j + 2][i] / 9 + imgarr[k + 2][j + 2][i] / 9)
 
-    newimgname = 'D:/aviris_hyperspectral_data/Gaussnoise_GaussianAve_200.tif'
+    newimgname = 'D:/aviris_hyperspectral_data/Gaussnoise_GaussianAve_300.tif'
     img = TIFF.open(newimgname, 'w')
     img.write_image(np.transpose(newimgarr), write_rgb=True)
     print('success!')
-
-    for i in range(3):
-        for j in range(2):
-            for k in range(5):
-                print(newimgarr[k, j, i], imgarr[k, j, i])
 
     '''
     print(type(imgarr[1, 1, 1]))
---
+++ src/get_every_layers.py
@@ -0,0 +1,64 @@
+# coding:GBK
+import numpy as np
+from libtiff import TIFF
+
+imgpath_original = 'D:/aviris_hyperspectral_data/19920612_AVIRIS_IndianPine_EW-line_R.tif'
+imgpath_noise = 'D:/aviris_hyperspectral_data/19920612_AVIRIS_IndianPine_EW-line_R_Gaussnoise.tif'
+imgpath_deeplearning = 'D:/aviris_hyperspectral_data/DeepLearn_300.tif'
+imgpath_midfil = 'D:/aviris_hyperspectral_data/Gaussnoise_Midfil_300.tif'
+imgpath_avefil = 'D:/aviris_hyperspectral_data/Gaussnoise_GaussianAve_300.tif'
+imgpath_gaussian_seta8 = 'D:/aviris_hyperspectral_data/Gaussnoise_Gaussian_Seta0.8_300.tif'
+
+
+save_path_original = 'D:/aviris_hyperspectral_data/all_layers_original/'
+save_path_noise = 'D:/aviris_hyperspectral_data/all_layers_noise/'
+save_path_deeplearning = 'D:/aviris_hyperspectral_data/all_layers_deeplearning/'
+save_path_midfil = 'D:/aviris_hyperspectral_data/all_layers_midfil/'
+save_path_avefil = 'D:/aviris_hyperspectral_data/all_layers_avefil/'
+save_path_gaussian_seta8 = 'D:/aviris_hyperspectral_data/all_layers_gaussian_seta8/'
+
+if __name__ == '__main__':
+    img_original_arr = TIFF.open(imgpath_original, mode="r").read_image()
+    img_noise_arr = TIFF.open(imgpath_noise, mode="r").read_image()
+    img_deeplearning_arr = TIFF.open(
+        imgpath_deeplearning, mode="r").read_image()
+    img_midfil_arr = TIFF.open(imgpath_midfil, mode="r").read_image()
+    img_avefil_arr = TIFF.open(imgpath_avefil, mode="r").read_image()
+    img_gaussian_seta8_arr = TIFF.open(
+        imgpath_gaussian_seta8, mode="r").read_image()
+    '''
+    #下面是原始图像的每一层
+    for i in range(220):
+        save_imgname = save_path_original + str(i) + '.tif'
+        img = TIFF.open(save_imgname, 'w')
+        img.write_image(img_original_arr[i], write_rgb=True)
+
+    # 下面是噪声图像的每一层
+    for i in range(220):
+        save_imgname = save_path_noise + str(i) + '.tif'
+        img = TIFF.open(save_imgname, 'w')
+        img.write_image(img_noise_arr[i], write_rgb=True)
+    
+    # 下面是得到深度神经网络后的每一层
+    for i in range(220):
+        save_imgname = save_path_deeplearning + str(i) + '.tif'
+        img = TIFF.open(save_imgname, 'w')
+        img.write_image(img_deeplearning_arr[i], write_rgb=True)
+    
+    # 下面是得到midfil后的每一层
+    for i in range(220):
+        save_imgname = save_path_midfil + str(i) + '.tif'
+        img = TIFF.open(save_imgname, 'w')
+        img.write_image(img_midfil_arr[i], write_rgb=True)
+    
+    # 下面是得到avefil后的每一层
+    for i in range(220):
+        save_imgname = save_path_avefil + str(i) + '.tif'
+        img = TIFF.open(save_imgname, 'w')
+        img.write_image(img_avefil_arr[i], write_rgb=True)
+    '''
+    # 下面是得到avefil后的每一层
+    for i in range(220):
+        save_imgname = save_path_gaussian_seta8 + str(i) + '.tif'
+        img = TIFF.open(save_imgname, 'w')
+        img.write_image(img_gaussian_seta8_arr[i], write_rgb=True)
